423d781146e3be4fe94e5f2a38c57d5d26262fb0
Chad Palmer <chad.n.palmer@gmail.com>
2021-06-09 14:30:34 -0500

    added database connection, config file with template, imported to server

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
