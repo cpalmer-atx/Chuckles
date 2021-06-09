e263b970ee0fd9077432d6edbf71af5b63edc55a
Chad Palmer <chad.n.palmer@gmail.com>
2021-06-09 16:19:00 -0500

    worked out bugs, passing postman tests, backend complete

--- controllers/data.js
+++ controllers/data.js
@@ -13,21 +13,21 @@ exports.getData = async (req, res) => {
     res.setHeader("Access-Control-Allow-Headers", "content-type");
     res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
 
-    return res.sendStatus(200).json({
+    return res.status(200).json({
       success: true,
       jokeCount: jokes.length,
       data: joke
     });
 
   } catch (error) {
-    return res.sendStatus(500).json({
+    return res.status(500).json({
       success: false,
       error: 'Server error encountered while retrieving joke.'
     });
   }
 }
 
-exports.addData = async (req, res) => {
+exports.postData = async (req, res) => {
   try {
     const joke = await Data.create(req.body);
 
@@ -37,15 +37,15 @@ exports.addData = async (req, res) => {
     res.setHeader("Access-Control-Allow-Headers", "content-type");
     res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
 
-    return res.sendStatus(201).json({
+    return res.status(201).json({
       success: true,
       data: joke
     });
 
   } catch (error) {
-    if (err.name === 'ValidationError') {
+    if (error.name === 'ValidationError') {
       const messages = Object.values(error.errors).map(val => val.messages);
-      res.sendStatus(400).json({
+      return res.status(400).json({
         success: false,
         error: messages
       });
@@ -64,22 +64,22 @@ exports.deleteData = async (req, res) => {
     const joke = await Data.findById(req.params.id);
 
     if (!joke) {
-      return res.sendStatus(404).json({
+      return res.status(404).json({
         success: true,
         error: `No joke with id ${req.params.id} found.`
       });
     }
 
     await joke.remove();
-    return res.sendStatus(200).json({
+    return res.status(200).json({
       success: true,
       data: {}
     });
-
+    
   } catch (error) {
-    return res.sendStatus(500).json({
+    return res.status(500).json({
       success: false,
-      error: 'Server error encountered while trying to delete joke.'
+      _error: 'Server error encountered while trying to delete joke.'
     });
   }
 }
